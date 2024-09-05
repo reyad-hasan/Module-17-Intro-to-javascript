8d0d4082f84f89f6d7797c2270597f5dbad94a01
reyad-hasan <akmkamrulhasanreyad@gmail.com>
2024-09-05 23:18:54 +0600

    js first module

---
+++ types.js
@@ -0,0 +1,47 @@
+// string
+// var name = 'Kamrul Hasan Reyad';
+// var country = 'Bangladesh';
+
+// boolean
+// var isRich = true;
+// var passed = false;
+// console.log(typeof passed);
+
+// number
+// var age = 29;
+// console.log(typeof age);
+
+// parseInt
+// var age = ('29');
+// var ageI = parseInt(age)
+// console.log(ageI);
+// var ageParseInt = parseInt('30');
+// console.log(ageParseInt)
+// console.log(parseInt(typeof age));
+
+// parseFloat
+// var age2 = ('22.5');
+// console.log(typeof age2);
+// var age2C = parseFloat(age2);
+// console.log(typeof age2C);
+// var float = parseFloat(age2);
+// console.log(typeof float)
+
+// toFixed.(); method
+// var first = 0.1;
+// var second = 0.2;
+// var total = first + second;
+// console.log(total);
+// console.log(total.toFixed(3));
+// var total2 = total.toFixed(3);
+// var totalP = parseFloat(total2);
+// console.log(total2);
+// console.log(totalP);
+// console.log(parseFloat(total.toFixed(5)));
+
+
+// infinity
+// const num = 50 / 0;
+// console.log(num);
+// const num2 = -50 / 0;
+// console.log(num);
---
+++ variable.js
@@ -0,0 +1,8 @@
+var age = 29;
+var weight = 58;
+
+
+var year = 1995;
+
+year = 1998;
+console.log(year);
